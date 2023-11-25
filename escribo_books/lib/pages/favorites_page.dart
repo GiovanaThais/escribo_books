66d249c4a2c719efcc2cec442b96bc0875721964
GiovanaThais <giovanatgc12@gmail.com>
2023-11-25 14:12:09 -0300

    create favorite page

--- escribo_books/lib/pages/favorites_page.dart
+++ escribo_books/lib/pages/favorites_page.dart
@@ -12,8 +12,31 @@ class _FavoritesPageState extends State<FavoritesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 78, 183, 224),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text("Favoritos"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 80,
+            margin: EdgeInsets.all(16),
+            width: double.infinity,
+            color: Color.fromARGB(255, 211, 245, 255),
+            child: const Card(
+              elevation: 7,
+              shadowColor: Color.fromARGB(255, 93, 223, 213),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text("Favorito"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
