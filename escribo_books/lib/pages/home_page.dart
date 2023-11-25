9a0e6652a15655f6fc2a0eee1d3509786ee007b9
GiovanaThais <giovanatgc12@gmail.com>
2023-11-25 14:12:38 -0300

    fix

--- escribo_books/lib/pages/home_page.dart
+++ escribo_books/lib/pages/home_page.dart
@@ -6,8 +6,8 @@ import 'package:escribo_books/stores/book_store.dart';
 import 'package:flutter/material.dart';
 import 'package:vocsy_epub_viewer/epub_viewer.dart';
 
-//import 'page1.dart';
-import 'page2.dart';
+import 'page1.dart';
+//import 'page2.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -19,222 +19,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final BookStore store = BookStore(
-      repository: BookRepository(
-    client: HttpClient(),
-  ));
   int pagePosition = 0;
   PageController controller = PageController(initialPage: 0);
 
   final pagesList = <Widget>[
-    //const Page1(),
-    const Page2(),
+    const Page1(
+      title: '',
+    ),
+    const FavoritesPage(),
   ];
-
-  @override
-  void initState() {
-    super.initState();
-    store.getBooks();
-  }
 
   final navigationBarList = const [
-    BottomNavigationBarItem(label: "Pag1", icon: Icon(Icons.home)),
-    BottomNavigationBarItem(label: "Pag2", icon: Icon(Icons.bookmark)),
+    BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
+    BottomNavigationBarItem(label: "Favoritos", icon: Icon(Icons.bookmark)),
   ];
-  Future<void> _downloadAndOpenBook(String downloadUrl) async {
-    try {
-      String filePath = await _downloadFile(downloadUrl);
-      _openEpubReader(filePath);
-    } catch (e) {
-      // Lógica para lidar com erros durante o download
-      print("Erro durante o download: $e");
-    }
-  }
-
-  Future<String> _downloadFile(String url) async {
-    // Lógica para download do arquivo
-
-    //url ficticia
-    return "/path/to/downloaded/book.epub";
-  }
-
-  void _openEpubReader(String filePath) {
-    VocsyEpub.setConfig(
-      themeColor: Theme.of(context).primaryColor,
-      identifier: "book_identifier",
-      scrollDirection: EpubScrollDirection.ALLDIRECTIONS,
-      allowSharing: true,
-      enableTts: true,
-      nightMode: false,
-    );
-
-    VocsyEpub.locatorStream.listen((locator) {
-      print('LOCATOR: $locator');
-    });
-
-    VocsyEpub.open(
-      filePath,
-      lastLocation: EpubLocator.fromJson({
-        "bookId": "book_identifier",
-        "href": "/OEBPS/ch06.xhtml",
-        "created": 1539934158390,
-        "locations": {"cfi": "epubcfi(/0!/4/4[simple_book]/2/2/6)"},
-      }),
-    );
-  }
-
-  bool isBookmarked = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      drawer: const CustomDrawerWidget(),
-      body: AnimatedBuilder(
-        animation: Listenable.merge([
-          store.isLoading,
-          store.erro,
-          store.state,
-        ]),
-        builder: (context, child) {
-          if (store.isLoading.value) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          if (store.erro.value.isNotEmpty) {
-            return Center(
-              child: Text(
-                store.erro.value,
-                style: const TextStyle(
-                  color: Color.fromARGB(136, 0, 0, 0),
-                  fontWeight: FontWeight.w600,
-                  fontSize: 20,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            );
-          }
-
-          if (store.state.value.isEmpty) {
-            return const Center(
-              child: Text(
-                'Nenhum item na lista',
-                style: TextStyle(
-                  color: Color.fromARGB(136, 4, 0, 0),
-                  fontWeight: FontWeight.w600,
-                  fontSize: 20,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            );
-          } else {
-            return ListView.separated(
-              separatorBuilder: (context, index) => const SizedBox(
-                height: 32,
-              ),
-              padding: const EdgeInsets.all(16),
-              itemCount: store.state.value.length,
-              itemBuilder: (_, index) {
-                final item = store.state.value[index];
-                return Column(
-                  children: [
-                    Stack(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16),
-                            border: Border.all(
-                              width: 1,
-                              color: Colors.grey, // Largura da borda
-                            ),
-                          ),
-                          child: InkWell(
-                            hoverColor: Colors.grey,
-                            onTap: () async {
-                              await _downloadAndOpenBook(item.download_url);
-                            },
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(16),
-                              child: Image.network(
-                                item.cover_url,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          right: -7,
-                          top: -13,
-                          child: IconButton(
-                              focusColor: Colors.white,
-                              onPressed: () {
-                                setState(() {
-                                  isBookmarked = true;
-                                });
-                              },
-                              icon: isBookmarked
-                                  ? const Icon(
-                                      Icons.bookmark,
-                                      color: Colors.red,
-                                      size: 40,
-                                    )
-                                  : const Icon(
-                                      Icons.bookmark_outline,
-                                      size: 40,
-                                    )
-
-                              //
-                              ),
-                        ),
-                      ],
-                    ),
-                    ListTile(
-                      contentPadding: EdgeInsets.zero,
-                      title: InkWell(
-                        onTap: () {},
-                        child: Text(
-                          item.title,
-                          style: const TextStyle(
-                            color: Color.fromARGB(136, 34, 89, 140),
-                            fontWeight: FontWeight.w600,
-                            fontSize: 24,
-                          ),
-                        ),
-                      ),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            item.author,
-                            style: const TextStyle(
-                              color: Color.fromARGB(255, 76, 89, 175),
-                              fontWeight: FontWeight.w600,
-                              fontSize: 20,
-                            ),
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            item.download_url,
-                            style: const TextStyle(
-                              color: Color.fromARGB(137, 108, 44, 44),
-                              fontWeight: FontWeight.w400,
-                              fontSize: 18,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                );
-              },
-            );
-          }
-        },
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(
+            widget.title,
+          ),
+        ),
+        drawer: const CustomDrawerWidget(),
+        body: PageView(
+          controller: controller,
+          onPageChanged: (value) {
+            setState(() {
+              pagePosition = value;
+            });
+          },
+          scrollDirection: Axis.vertical,
+          children: pagesList,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+            onTap: (value) {
+              controller.jumpToPage(value);
+            },
+            currentIndex: pagePosition,
+            items: navigationBarList),
       ),
     );
   }
--- escribo_books/lib/pages/page1.dart
+++ escribo_books/lib/pages/page1.dart
@@ -1,34 +1,229 @@
+import 'package:escribo_books/model/book_model.dart';
 import 'package:flutter/material.dart';
+import 'package:escribo_books/http/http_client.dart';
+import 'package:escribo_books/pages/favorites_page.dart';
+import 'package:escribo_books/repositories/book_repository.dart';
+import 'package:escribo_books/repositories/custom_drawer_widget.dart';
+import 'package:escribo_books/stores/book_store.dart';
+import 'package:vocsy_epub_viewer/epub_viewer.dart';
 
 class Page1 extends StatefulWidget {
-  const Page1({super.key});
+  const Page1({super.key, required this.title});
+
+  final String title;
 
   @override
   State<Page1> createState() => _Page1State();
 }
 
 class _Page1State extends State<Page1> {
+  final BookStore store = BookStore(
+      repository: BookRepository(
+    client: HttpClient(),
+  ));
+  Future<void> _downloadAndOpenBook(String downloadUrl) async {
+    try {
+      String filePath = await _downloadFile(downloadUrl);
+      _openEpubReader(filePath);
+    } catch (e) {
+      // Lógica para lidar com erros durante o download
+      print("Erro durante o download: $e");
+    }
+  }
+
+  Future<String> _downloadFile(String url) async {
+    // Lógica para download do arquivo
+
+    //url ficticia
+    return "/path/to/downloaded/book.epub";
+  }
+
+  void _openEpubReader(String filePath) {
+    VocsyEpub.setConfig(
+      themeColor: Theme.of(context).primaryColor,
+      identifier: "book_identifier",
+      scrollDirection: EpubScrollDirection.ALLDIRECTIONS,
+      allowSharing: true,
+      enableTts: true,
+      nightMode: false,
+    );
+
+    VocsyEpub.open(
+      filePath,
+      lastLocation: EpubLocator.fromJson({
+        "bookId": "book_identifier",
+        "href": "/OEBPS/ch06.xhtml",
+        "created": 1539934158390,
+        "locations": {"cfi": "epubcfi(/0!/4/4[simple_book]/2/2/6)"},
+      }),
+    );
+    VocsyEpub.locatorStream.listen((locator) {
+      print('LOCATOR: $BookRepository');
+    });
+  }
+
+  bool isBookmarked = false;
+
+  final List<BookModel> _favoritesList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    store.getBooks();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          margin: EdgeInsets.all(16),
-          width: double.infinity,
-          child: const Card(
-              elevation: 7,
-              shadowColor: Colors.green,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text("Meu card"),
-                  ),
-                ],
-              )),
-        ),
-      ],
+    return Scaffold(
+      body: AnimatedBuilder(
+        animation: Listenable.merge([
+          store.isLoading,
+          store.erro,
+          store.state,
+        ]),
+        builder: (context, child) {
+          if (store.isLoading.value) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (store.erro.value.isNotEmpty) {
+            return Center(
+              child: Text(
+                store.erro.value,
+                style: const TextStyle(
+                  color: Color.fromARGB(136, 0, 0, 0),
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+
+          if (store.state.value.isEmpty) {
+            return const Center(
+              child: Text(
+                'Nenhum item na lista',
+                style: TextStyle(
+                  color: Color.fromARGB(136, 4, 0, 0),
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            );
+          } else {
+            return ListView.separated(
+              separatorBuilder: (context, index) => const SizedBox(
+                height: 32,
+              ),
+              padding: const EdgeInsets.all(16),
+              itemCount: store.state.value.length,
+              itemBuilder: (_, index) {
+                final isFavorite =
+                    _favoritesList.contains(store.state.value[index]);
+                final item = store.state.value[index];
+                return Column(
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            border: Border.all(
+                              width: 1,
+                              color: Colors.grey, // Largura da borda
+                            ),
+                          ),
+                          child: InkWell(
+                            hoverColor: Colors.grey,
+                            onTap: () async {
+                              await _downloadAndOpenBook(item.download_url);
+                            },
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(16),
+                              child: Image.network(
+                                item.cover_url,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          right: -7,
+                          top: -13,
+                          child: IconButton(
+                            focusColor: Colors.white,
+                            onPressed: () {
+                              setState(() {
+                                //  isBookmarked = true;
+                                if (isFavorite) {
+                                  _favoritesList
+                                      .remove(store.state.value[index]);
+                                } else {
+                                  _favoritesList.add(store.state.value[index]);
+                                }
+                              });
+                            },
+                            icon: isFavorite
+                                ? const Icon(
+                                    Icons.bookmark,
+                                    color: Colors.red,
+                                    size: 40,
+                                  )
+                                : const Icon(
+                                    Icons.bookmark_outline,
+                                    size: 40,
+                                  ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      title: InkWell(
+                        onTap: () {},
+                        child: Text(
+                          item.title,
+                          style: const TextStyle(
+                            color: Color.fromARGB(134, 19, 132, 177),
+                            fontWeight: FontWeight.w600,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            item.author,
+                            style: const TextStyle(
+                              color: Color.fromARGB(255, 76, 130, 175),
+                              fontWeight: FontWeight.w600,
+                              fontSize: 20,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            item.download_url,
+                            style: const TextStyle(
+                              color: Color.fromARGB(133, 1, 107, 255),
+                              fontWeight: FontWeight.w400,
+                              fontSize: 18,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
